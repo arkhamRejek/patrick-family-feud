3c81936b33bac0cbb9e9f1657958153f9eb3bce6
Paul-E <paulbrunache@gmail.com>
2025-07-09 17:26:17 -0400

    deploy updates

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.NODE_ENV === "production" ? "/patrick-family-feud" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/patrick-family-feud" : "",
   eslint: {
     ignoreDuringBuilds: true,
   },
